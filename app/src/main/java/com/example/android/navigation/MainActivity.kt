c6a78ca2da6010b795a7175005e7fb3b9fb98341
toby1907 <olaleyetobi3@gmail.com>
2021-03-26 14:02:34 +0100

    Up button configuration using navigation

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -32,12 +32,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        // TODO (01) Find the navController from myNavHostFragment
+
+       val navController = this.findNavController(R.id.myNavHostFragment)
         // Since we're using KTX, you can call this.findNavController
-        // TODO (02) Link the navController to our ActionBar
+
+        NavigationUI.setupActionBarWithNavController(this,navController)
+
         // By calling NavigationUI.setupActionBarWithNavController
     }
 
-    // TODO (03) Override onSupportNavigateUp
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
+    }
+
     // Find the navController and then call navController.navigateUp
 }
